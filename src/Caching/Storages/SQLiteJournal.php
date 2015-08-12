9512ca53e9e382ff41c84db0e6e9b6b07eb87370
Miloslav Hůla <miloslav.hula@gmail.com>
2015-08-12 22:02:42 +0200

    SQLiteJournal: filesystem permissions like ordinary file [Closes #29]

--- src/Caching/Storages/SQLiteJournal.php
+++ src/Caching/Storages/SQLiteJournal.php
@@ -38,6 +38,10 @@ class SQLiteJournal extends Nette\Object implements IJournal
 			throw new Nette\NotSupportedException('SQLiteJournal requires PHP extension pdo_sqlite which is not loaded.');
 		}
 
+		if ($this->path !== ':memory:' && !is_file($this->path)) {
+			touch($this->path); // ensures ordinary file permissions
+		}
+
 		$this->pdo = new \PDO('sqlite:' . $this->path);
 		$this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 		$this->pdo->exec('
