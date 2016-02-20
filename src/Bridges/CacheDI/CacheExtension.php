819220f6f8dab1610793939a4fafa860c600e8ec
David Grudl <david@grudl.com>
2016-02-20 10:05:12 +0100

    CacheExtension: journal service SQLiteJournal is added only when extension pdo_sqlite is loaded

--- src/Bridges/CacheDI/CacheExtension.php
+++ src/Bridges/CacheDI/CacheExtension.php
@@ -29,16 +29,20 @@ class CacheExtension extends Nette\DI\CompilerExtension
 	{
 		$container = $this->getContainerBuilder();
 
-		$container->addDefinition($this->prefix('journal'))
-			->setClass(Nette\Caching\Storages\IJournal::class)
-			->setFactory(Nette\Caching\Storages\SQLiteJournal::class, [$this->tempDir . '/cache/journal.s3db']);
+		if (extension_loaded('pdo_sqlite')) {
+			$container->addDefinition($this->prefix('journal'))
+				->setClass(Nette\Caching\Storages\IJournal::class)
+				->setFactory(Nette\Caching\Storages\SQLiteJournal::class, [$this->tempDir . '/cache/journal.s3db']);
+		}
 
 		$container->addDefinition($this->prefix('storage'))
 			->setClass(Nette\Caching\IStorage::class)
 			->setFactory(Nette\Caching\Storages\FileStorage::class, [$this->tempDir . '/cache']);
 
 		if ($this->name === 'cache') {
-			$container->addAlias('nette.cacheJournal', $this->prefix('journal'));
+			if (extension_loaded('pdo_sqlite')) {
+				$container->addAlias('nette.cacheJournal', $this->prefix('journal'));
+			}
 			$container->addAlias('cacheStorage', $this->prefix('storage'));
 		}
 	}
