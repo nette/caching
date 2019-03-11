f0c45557ed36f7dda23702056808ca50b7a25939
David Grudl <david@grudl.com>
2019-03-11 23:55:16 +0100

    typo

--- src/Bridges/CacheDI/CacheExtension.php
+++ src/Bridges/CacheDI/CacheExtension.php
@@ -40,12 +40,12 @@ final class CacheExtension extends Nette\DI\CompilerExtension
 		if (extension_loaded('pdo_sqlite')) {
 			$builder->addDefinition($this->prefix('journal'))
 				->setType(Nette\Caching\Storages\IJournal::class)
-				->setFactory(Nette\Caching\Storages\SQLiteJournal::class, [$this->tempDir . '/cache/journal.s3db']);
+				->setFactory(Nette\Caching\Storages\SQLiteJournal::class, [$dir . '/journal.s3db']);
 		}
 
 		$builder->addDefinition($this->prefix('storage'))
 			->setType(Nette\Caching\IStorage::class)
-			->setFactory(Nette\Caching\Storages\FileStorage::class, [$this->tempDir . '/cache']);
+			->setFactory(Nette\Caching\Storages\FileStorage::class, [$dir]);
 
 		if ($this->name === 'cache') {
 			if (extension_loaded('pdo_sqlite')) {
