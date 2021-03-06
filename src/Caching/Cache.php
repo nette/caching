f66b8f902a9efb5a53a164cec15db7aaa938a333
David Grudl <david@grudl.com>
2021-03-06 15:40:30 +0100

    coding style

--- src/Caching/Cache.php
+++ src/Caching/Cache.php
@@ -124,10 +124,8 @@ class Cache
 				$result[$key] = $this->load(
 					$key,
 					$generator
-						? function (&$dependencies) use ($key, $generator) {
-							return $generator(...[$key, &$dependencies]);
-						}
-						: null
+						? fn(&$dependencies) => $generator(...[$key, &$dependencies])
+						: null,
 				);
 			}
 
@@ -141,9 +139,7 @@ class Cache
 			if (isset($cacheData[$storageKey])) {
 				$result[$key] = $cacheData[$storageKey];
 			} elseif ($generator) {
-				$result[$key] = $this->load($key, function (&$dependencies) use ($key, $generator) {
-					return $generator(...[$key, &$dependencies]);
-				});
+				$result[$key] = $this->load($key, fn(&$dependencies) => $generator(...[$key, &$dependencies]));
 			} else {
 				$result[$key] = null;
 			}
@@ -285,9 +281,7 @@ class Cache
 			$key[0][0] = get_class($function[0]);
 		}
 
-		return $this->load($key, function () use ($function, $key) {
-			return $function(...array_slice($key, 1));
-		});
+		return $this->load($key, fn() => $function(...array_slice($key, 1)));
 	}
 
 
