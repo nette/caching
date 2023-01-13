7c37a975a0d1077c845fb08f446095ca33b101ac
David Grudl <david@grudl.com>
2023-01-13 04:48:57 +0100

    deprecated stuff

--- src/Caching/Cache.php
+++ src/Caching/Cache.php
@@ -65,9 +65,7 @@ class Cache
 	public const ALL = self::All;
 
 	/** @internal */
-	public const
-		NamespaceSeparator = "\x00",
-		NAMESPACE_SEPARATOR = self::NamespaceSeparator;
+	public const NamespaceSeparator = "\x00";
 
 	private Storage $storage;
 	private string $namespace;
@@ -386,6 +384,7 @@ class Cache
 	 */
 	public function start($key): ?OutputHelper
 	{
+		trigger_error(__METHOD__ . '() was renamed to capture()', E_USER_DEPRECATED);
 		return $this->capture($key);
 	}
 
